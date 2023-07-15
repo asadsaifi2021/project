b9e8382115c8233f44469121f74078c57600737a
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2023-07-15 06:33:43 +0530

    Update firebase config

--- firebase.config.js
+++ firebase.config.js
@@ -3,13 +3,13 @@ import { getFireStore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8Ayl-DISGiZwS3J8KOb5tXJW1OwA64oo",
-  authDomain: "media-page-e0648.firebaseapp.com",
-  databaseURL: "https://media-page-e0648-default-rtdb.firebaseio.com",
-  projectId: "media-page-e0648",
-  storageBucket: "media-page-e0648.appspot.com",
-  messagingSenderId: "530330106531",
-  appId: "1:530330106531:web:28c029993aed5b342edd16",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
